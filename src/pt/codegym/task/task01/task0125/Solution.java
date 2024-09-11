741f73f152645d62c89cbc1be89b5b83cc4a3f97
beral98 <morrinho1998@gmail.com>
2024-09-11 00:56:30 -0300

    one more task

---
+++ src/pt/codegym/task/task01/task0125/Solution.java
@@ -0,0 +1,18 @@
+package pt.codegym.task.task01.task0125;
+
+/* 
+A grande limpeza
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //String s = "15";
+        int a = 5;
+        //int z = 18;
+        int d = 18;
+        int c = a + d;
+        String b = " é melhor do que ";
+
+        System.out.println(a + b + c);
+    }
+}
